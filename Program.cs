c21044584be2c272abf0b78a5f2ddfbbe7573d5d
Estyvison Linhares <titoalbuquerque34@gmail.com>
2024-05-07 16:58:45 -0300

    Operadores no dotnet

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 ﻿using FundamentosDotnet.Models;
+using fundametos_dotnet.Models;
 
 Pessoa pessoa = new Pessoa();
 pessoa.Nome = "Tito";
 pessoa.Idade = 10;
 
 pessoa.Apresentar();
+
+Operadores operadores = new Operadores();
+operadores.OperadoresExemplo();
