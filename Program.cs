af67aa750e5f45d62da5c0ca3512d11a13168111
Estyvison Linhares <titoalbuquerque34@gmail.com>
2024-05-07 09:18:39 -0300

    Aula de classes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using fundametos_dotnet.Models;
+
 Console.WriteLine("Hello, World!");
+Pessoa pessoa = new Pessoa();
+pessoa.Apresentar();
